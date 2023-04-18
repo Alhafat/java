c68bd3cd09d9a140f7ab2243e091005104ff87c9
Alhafat <vladimir-lukin@inbox.ru>
2023-04-18 22:32:00 +0300

    18.04.2023

--- lesson/lesson_2/seminar/exp_7.java
+++ lesson/lesson_2/seminar/exp_7.java
@@ -1,6 +1,11 @@
 package lesson.lesson_2.seminar;
 
 import java.io.FileWriter;
+import java.io.IOException;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 public class exp_7 {
 
@@ -9,17 +14,32 @@ public class exp_7 {
         int n = 5;
         SaveFile(RepeatString(text, n));
     }
+
     static void SaveFile(String s) {
-        String path = "test.txt";
-        try (FileWriter fw = new FileWriter(path, true);){
+        Logger logger = Logger.getAnonymousLogger();
+        FileHandler fileHandler = null;
+        try {
+            fileHandler = new FileHandler("txt");
+        } catch (IOException e) {
+            e.printStackTrace(); //ловим все ошибки
+        }
+        SimpleFormatter formatter = new SimpleFormatter();
+        fileHandler.setFormatter(formatter);
+        logger.addHandler(fileHandler);
+        String path = "n/n/n/test.txt";
+       try (FileWriter fw = new FileWriter(path, true);){
             fw.write(s); // Подготовка к записи в файл
             fw.write('\n');
             fw.flush(); // Запись в файл            
         } catch (Exception e) {
             e.printStackTrace(); //ловим все ошибки
+            logger.log(Level.WARNING, "Что-то пошло не так");
         } 
+        System.out.println("Тест");
+        fileHandler.close();
     }
-    static String RepeatString (String s, int n) {
+
+    static String RepeatString(String s, int n) {
         return s.repeat(n);
-    }    
+    }
 }
