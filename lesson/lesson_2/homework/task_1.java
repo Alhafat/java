3a2b4342d596b072d0edee49cb848ecadcd95241
Alhafat <vladimir-lukin@inbox.ru>
2023-04-21 17:01:36 +0300

    21.04.2023

--- lesson/lesson_2/homework/task_1.java
+++ lesson/lesson_2/homework/task_1.java
@@ -1,12 +1,8 @@
 package lesson.lesson_2.homework;
 
-import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
