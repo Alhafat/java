4d75b7d25668930ff925e32ecf841db6b1864e7e
Alhafat <vladimir-lukin@inbox.ru>
2023-05-19 20:28:16 +0300

    19.05.2023

---
+++ lesson/lesson_6/lesson/worker.java
@@ -0,0 +1,9 @@
+package java.lesson.lesson_6.lesson;
+
+public class worker {
+    int id;
+    int salary;
+    String first_name;
+    String last_name;
+    
+}
---
+++ lesson/lesson_6/seminar/exp_3.java
@@ -0,0 +1,7 @@
+package lesson.lesson_6.seminar;
+
+public class exp_3 {
+    public static void main(String[] args) {
+        
+    }
+}
